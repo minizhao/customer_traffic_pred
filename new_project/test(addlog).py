380e0ef3b0bae38aaca10c68c1d206485de563e0
minizhao <zhao844895171@gmail.com>
2017-10-21 02:09:44 +0800

    Update test(addlog).py

--- new_project/test(addlog).py
+++ new_project/test(addlog).py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import data_helper
 from sklearn.externals import joblib
-import time,datetime
+import time, datetime
 import random
 import pandas as pd
 from flask import *
@@ -16,171 +16,181 @@ logging.basicConfig(level=logging.DEBUG,
 
 
 app = Flask(__name__)
-# http://127.0.0.1:5000/analysis?function=traffic&storeId=731888568958976&hour=9,20&daySpan=2017-09-20,2017-9-25
+
+# http://127.0.0.1:5000/analysis?function=traffic&storeId=731888568958976&hour=9-20&daySpan=2017-09-20,2017-9-25
 @app.route('/analysis/', methods=['GET'])
-
-
 def analyse():
-	
-	error='ok'
-		
-	time_point = request.args.get('hour') #预测几点
+    error = 'ok'
+    # print(request.args.get('hour'))
 
-	dates = request.args.get('daySpan') #预测几月到几月
-	function = request.args.get('function')
-	storeId = request.args.get('storeId')
+    time_point = request.args.get('hour')  # 预测几点
 
-	#判断什么也没有输入
-	#http://127.0.0.1:5000/analysis/
-	if time_point == None and dates == None and function== None and storeId== None :
+    dates = request.args.get('daySpan')  # 预测几月到几月
+    function = request.args.get('function')
+    storeId = request.args.get('storeId')
 
-		storeId=731888568958976#默认店铺
-
-		function ='customer'#默认计算客流量
-
-		now = datetime.datetime.now()
-
-		str_now=now.strftime('%Y-%m-%d %H:%M:%S')  
-		date_now=str_now.split(' ')[0]
-		hour_point_now=int(str(str_now.split(' ')[1]).split(':')[0])
-		if ((hour_point_now+1)<9 and (hour_point_now+1)>22):
-
-			now_day=datetime.date(*[int(x) for x in str(date_now).split('-')])
-			tomorrow=now_day+datetime.timedelta(days=i)
-			dates=[tomorrow.strftime('%Y-%m-%d')]
-			time=int(9)
+    # 判断什么也没有输入
+    # http://127.0.0.1:5000/analysis/
+    if time_point == None and dates == None and function == None and storeId == None:
 
 
-		else:
-			dates=[date_now]
-			time=int(hour_point_now+1)
+        storeId = 731888568958976  # 默认店铺
 
-		d_start = datetime.date(*[int(x) for x in str(dates[0]).split('-')])
+        function = ['traffic','customer']  # 默认计算客流量
 
-		d_to_str=d_start.strftime('%Y-%m-%d')
-		result=get_reslut(d_to_str,time,storeId,function)
+        now = datetime.datetime.now()
 
-		radm=random.randint(0, 50)
-		output=(result+radm)
-		result_dic=dict()
-		result_dic[str(d_to_str)+','+str(time)]=str(int(output))
-		# df=pd.DataFrame()
+        str_now = now.strftime('%Y-%m-%d %H:%M:%S')
+        date_now = str_now.split(' ')[0]
+        hour_point_now = int(str(str_now.split(' ')[1]).split(':')[0])
+        if ((hour_point_now + 1) < 9 or (hour_point_now + 1) > 22):
 
-		resp = { "msg":error,"code":0,"data": result_dic}
-		logging.info('ok')
-		return jsonify(resp)
+            now_day = datetime.date(*[int(x) for x in str(date_now).split('-')])
+            tomorrow = now_day + datetime.timedelta(days=1)
+            dates = [tomorrow.strftime('%Y-%m-%d')]
+            time = int(9)
 
-		
-	
-	try:
-		time_point = [int(x) for x in str(request.args.get('hour')).split(',') ] #预测几点
-	except ValueError as e:
-	 	error = "Incorrect input data <hour>: "
-	 	error+=( ': '+str(e)  )
-		resp = { "msg":error,"code":1,"data": None}
-		logging.debug(error)
-		return jsonify(resp)
+        else:
+            dates = [date_now]
+            time = int(hour_point_now + 1)
 
-	try:
-		dates = str(request.args.get('daySpan')).split(',')  #预测几月到几月
-	except ValueError as e:
-	 	error = "Incorrect input data <dates>: "
-	 	error+=( ': '+str(e)  )
-	 	logging.debug(error)
-		resp = { "msg":error,"code":1,"data": None}
-		return jsonify(resp)
+        d_start = datetime.date(*[int(x) for x in str(dates[0]).split('-')])
 
-	try:
-		function = str(request.args.get('function'))
+        d_to_str = d_start.strftime('%Y-%m-%d')
+        result1 = get_reslut(d_to_str, time, storeId, function[0])
+        result2 = get_reslut(d_to_str, time, storeId, function[-1])
+        radm = random.randint(0, 50)
+        output=function[0]+':'+str(int(result1+radm))+','+function[-1]+':'+str(int(result2+radm))
+        result_dic = dict()
+        result_dic[str(d_to_str) + ',' + str(time)] = str(output)
+        # df=pd.DataFrame()
 
-	except ValueError as e:
-	 	error = "Incorrect input data <function>: "
-	 	error+=( ': '+str(e)  )
-	 	logging.debug(error)
-		resp = { "msg":error,"code":1,"data": None}
-		return jsonify(resp)
+        resp = {"msg": error, "code": 0, "data": result_dic}
+        logging.info('ok')
+        return jsonify(resp)
 
-	try:
-		storeId = int(str(request.args.get('storeId')))
-
-	except ValueError as e:
-	 	error = "Incorrect input data <storeId>: "
-	 	error+=( ': '+str(e)  )
-	 	logging.debug(error)
-		resp = { "msg":error,"code":1,"data": None}
-		return jsonify(resp)
-
-	
-
-	if function not in ['traffic','customer']:
-		error = "Incorrect parameter input <function>: "
-		logging.debug(error)
-		resp = { "msg":error,"code":1,"data": None}
-		return jsonify(resp)
-
-	if function not in ['traffic','customer']:
-		error = "Incorrect parameter input <function>: "
-		logging.debug(error)
-		resp = { "msg":error,"code":1,"data": None}
-		return jsonify(resp)
-
-	
-	# method=1  #method 0 or 1 ,method 0:精确预测，耗时多; method 1:历史数据预测，耗时少;
-	try:
-		d_start = datetime.date(*[int(x) for x in str(dates[0]).split('-')])
-		d_end= datetime.date(*[int(x) for x in str(dates[-1]).split('-')])
-	except ValueError as e:
-	 	error = "Incorrect input data : "
-	 	error+=( ': '+str(e)  )
-	 	logging.debug(error)
-		resp = { "msg":error,"code":1,"data": None}
-		return jsonify(resp)
-
-	if d_start>d_end:
-		error = "Incorrect parameter input <dates> :The start date must be before the end date: "
-		logging.debug(error)
-		resp = { "msg":error,"code":1,"data": None}
-		return jsonify(resp)
-
-	days_span=(d_end-d_start).days #差了几天
-	
-	# df=pd.DataFrame()
-
-	result_dic=dict()
-	for i in range(days_span+1):
-		d=d_start+datetime.timedelta(days=i)
-		d_to_str=d.strftime('%Y-%m-%d')
-		# print d_to_str
-		for time in range(time_point[0],time_point[-1]+1):
-
-			result=get_reslut(d_to_str,time,storeId,function)
-		
-			radm=random.randint(0, 50)
-			output=(result+radm)
-
-			result_dic[str(d_to_str)+','+str(time)]=str(int(output))
-
-	resp = { "msg":error,"code":0,"data": result_dic}
-	logging.info('ok')
-	return jsonify(resp)
+    try:
+        time_point = [int(x) for x in str(request.args.get('hour')).split(',')]  # 预测几点
+    except ValueError as e:
+        error = "Incorrect input data <hour>: "
+        error += (': ' + str(e))
+        resp = {"msg": error, "code": 1, "data": None}
+        logging.debug(error)
+        return jsonify(resp)
 
 
-def get_reslut(d_to_str,time,storeId,pred):
-
-	args=dict()
-	args['storeId']=storeId
-	args['starttime']=time
-	args['preds']=pred #人流客流特征参数 'traffic'/'customer'
-	args['day']=d_to_str
-
-	args_pref_features=data_helper.data_loader(train=False,args=args)
-	model= joblib.load('train_model.m')
-	return model.predict([args_pref_features])
-	
 
 
+    try:
+        time_point = [int(x) for x in str(request.args.get('hour')).split('-')]  # 预测几点
+    except ValueError as e:
+        error = "Incorrect input data <hour>: "
+        error += (': ' + str(e))
+        resp = {"msg": error, "code": 1, "data": None}
+        logging.debug(error)
+        return jsonify(resp)
+
+    try:
+        dates = str(request.args.get('daySpan')).split(',')  # 预测几月到几月
+    except ValueError as e:
+        error = "Incorrect input data <dates>: "
+        error += (': ' + str(e))
+        logging.debug(error)
+        resp = {"msg": error, "code": 1, "data": None}
+        return jsonify(resp)
+
+    try:
+        function_ = str(request.args.get('function')).split(',')
+
+    except ValueError as e:
+        error = "Incorrect input data <function>: "
+        error += (': ' + str(e))
+        logging.debug(error)
+        resp = {"msg": error, "code": 1, "data": None}
+        return jsonify(resp)
+
+    try:
+        storeId = int(str(request.args.get('storeId')))
+
+    except ValueError as e:
+        error = "Incorrect input data <storeId>: "
+        error += (': ' + str(e))
+        resp = {"msg": error, "code": 1, "data": None}
+        logging.debug(error)
+        return jsonify(resp)
+
+    if time_point == None or dates == None or function_ == None or storeId == None:
+        error = "Missing input data: "
+        resp = {"msg": error, "code": 1, "data": None}
+        logging.debug(error)
+        return jsonify(resp)
+
+
+    if function_[0]  not in ['traffic', 'customer'] or function_[-1]  not in ['traffic', 'customer']:
+        error = "Incorrect parameter input <function>"+function_[0]+','+function_[-1]+": "
+        resp = {"msg": error, "code": 1, "data": None}
+        logging.debug(error)
+        return jsonify(resp)
+
+
+
+    # method=1  #method 0 or 1 ,method 0:精确预测，耗时多; method 1:历史数据预测，耗时少;
+    try:
+        d_start = datetime.date(*[int(x) for x in str(dates[0]).split('-')])
+        d_end = datetime.date(*[int(x) for x in str(dates[-1]).split('-')])
+    except ValueError as e:
+        error = "Incorrect input data : "
+        error += (': ' + str(e))
+        resp = {"msg": error, "code": 1, "data": None}
+        logging.debug(error)
+        return jsonify(resp)
+
+    if d_start > d_end:
+        error = "Incorrect parameter input <dates> :The start date must be before the end date: "
+        resp = {"msg": error, "code": 1, "data": None}
+        logging.debug(error)
+        return jsonify(resp)
+
+    days_span = (d_end - d_start).days  # 差了几天
+
+    # df=pd.DataFrame()
+
+    result_dic = dict()
+    for i in range(days_span + 1):
+        d = d_start + datetime.timedelta(days=i)
+        d_to_str = d.strftime('%Y-%m-%d')
+        # print d_to_str
+        for time in range(time_point[0], time_point[-1] + 1):
+            result=[]
+            for fun in function_:
+                if fun in ['traffic', 'customer']:
+                    temp_result = get_reslut(d_to_str, time, storeId, fun)
+                    radm = random.randint(0, 50)
+                    if fun=='traffic':
+                        result.append('traffic:'+str(int(temp_result)+radm))
+                    if fun=='customer':
+                        result.append('customer:'+str(int(temp_result)+radm))
+
+            output = ','.join(result)
+
+            result_dic[str(d_to_str) + '-' + str(time)] = str((output))
+
+    resp = {"msg": error, "code": 0, "data": result_dic}
+    logging.info('ok')
+    return jsonify(resp)
+
+
+def get_reslut(d_to_str, time, storeId, pred):
+    args = dict()
+    args['storeId'] = storeId
+    args['starttime'] = time
+    args['preds'] = pred  # 人流客流特征参数 'traffic'/'customer'
+    args['day'] = d_to_str
+
+    args_pref_features = data_helper.data_loader(train=False, args=args)
+    model = joblib.load('train_model.m')
+    return model.predict([args_pref_features])
 
 
 if __name__ == '__main__':
-
-	app.run(host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0')
